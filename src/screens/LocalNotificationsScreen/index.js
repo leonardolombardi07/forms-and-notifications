eef8e837702171e1411893bdc96130566b09466a
leonardolombardi07 <leonardolombardi@poli.ufrj.br>
2020-07-23 12:40:03 -0300

    Code refactored

--- src/screens/LocalNotificationsScreen/index.js
+++ src/screens/LocalNotificationsScreen/index.js
@@ -1,70 +1,16 @@
 import React, { useEffect } from "react";
 import { SafeAreaView, Text, Button, StyleSheet } from "react-native";
-
-import * as Permissions from "expo-permissions";
 import * as Notifications from "expo-notifications";
 
-// Essa parte do código garante que um usuário recebe uma notificação
-// mesmo se estiver em outro aplicativo
-Notifications.setNotificationHandler({
-  handleNotification: async () => {
-    return {
-      shouldShowAlert: true,
-    };
-  },
-});
+import useNotifications from "../../hooks/useNotifications";
 
-const LocalNotificationsScreen = ({ navigation }) => {
-  useEffect(() => {
-    // getPermissions verifica se um usuário tem permissão pra receber notificações
-    // se não tiver, uma solicitação para ter permissão é mandada
-    // se não der permissão, um alerta aparece
-    // apenas para IOS
-    const getPermissions = async () => {
-      const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-      if (status !== "granted") {
-        const { status: askedStatus } = await Permissions.askAsync(
-          Permissions.NOTIFICATIONS
-        );
-        if (!askedStatus !== "granted") {
-          return alert("Permissao não concedida");
-        }
-      }
-    };
-    getPermissions();
-  }, []);
+const LocalNotificationsScreen = () => {
+  const [
+    hasNotificationsPermission,
+    loadingNotificationsPermission,
+  ] = useNotifications();
 
-  useEffect(() => {
-    // foregroundSubscription identifica quando uma notificação é enviada
-    // com o usuário dentro do app
-    const foregroundSubscription = Notifications.addNotificationReceivedListener(
-      (notification) => {
-        // esse código é executado com dados da notificação
-        navigation.navigate("FormikScreen");
-      }
-    );
-
-    return () => {
-      foregroundSubscription.remove();
-    };
-  }, []);
-
-  useEffect(() => {
-    // foregroundSubscription identifica quando uma notificação é enviada
-    // com o usuário fora do app
-    const backgroundSubscription = Notifications.addNotificationResponseReceivedListener(
-      (response) => {
-        // esse código é executado quando usuário clica na notificação
-        navigation.navigate("UnformScreen");
-      }
-    );
-
-    return () => {
-      backgroundSubscription.remove();
-    };
-  }, []);
-
-  const triggerNotificationHandler = async () => {
+  const handleLocalNotification = async () => {
     await Notifications.scheduleNotificationAsync({
       content: {
         title: "Primeira Notificação Local",
@@ -76,12 +22,13 @@ const LocalNotificationsScreen = ({ navigation }) => {
       },
     });
   };
+
   return (
     <SafeAreaView style={styles.screenContainer}>
       <Text>LocalNotificationsScreen</Text>
       <Button
-        title="Trigger Notification"
-        onPress={triggerNotificationHandler}
+        title="Enviar Notificação Local"
+        onPress={handleLocalNotification}
       />
     </SafeAreaView>
   );
---
+++ src/services/apis/index.js
@@ -0,0 +1,1 @@
+export * from "./expoPushApi";
