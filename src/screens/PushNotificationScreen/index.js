fb20eda9c85b9c77c6fd1d3312468d415c4aa5b4
leonardolombardi07 <leonardolombardi@poli.ufrj.br>
2020-07-23 10:21:44 -0300

    Local Notification working

--- src/screens/PushNotificationScreen/index.js
+++ src/screens/PushNotificationScreen/index.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaView, Text, Button, StyleSheet } from "react-native";
 
+import * as Permissions from "expo-permissions";
 import * as Notifications from "expo-notifications";
 
 const PushNotificationsScreen = () => {
-  const triggerNotificationHandler = () => {
-    Notifications.scheduleNotificationAsync({
+  const triggerNotificationHandler = async () => {
+    await Notifications.scheduleNotificationAsync({
       content: {
         title: "Primeira Notificação Local",
         body: "Essa é a primeira notificação local que eu mando (:",
         color: "red",
       },
       trigger: {
-        seconds: 10,
+        seconds: 5,
       },
     });
   };
+
+  useEffect(() => {
+    const getPermissions = async () => {
+      const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+      if (status !== "granted") {
+        const { status: askedStatus } = await Permissions.askAsync(
+          Permissions.NOTIFICATIONS
+        );
+        if (!askedStatus !== "granted") {
+          return alert("Permissao não concedida");
+        }
+      }
+    };
+    getPermissions();
+  }, []);
   return (
     <SafeAreaView style={styles.screenContainer}>
       <Text>PushNotificationsScreen</Text>
